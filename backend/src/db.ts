77cdf598af330ca2269b6106428d22056c64b7a9
juancruzgarciaf <130694398+juancruzgarciaf@users.noreply.github.com>
2026-03-29 20:01:23 -0300

    robenme

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,12 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "gymmaxxing_db",
-  password: "26062011",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
